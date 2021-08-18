26ff6b5308c359cb6d8140182261e06d13f95537
Hawleywoo <alhawley21@gmail.com>
2021-08-17 22:43:56 -0600

    More helpers

--- radixSort.js
+++ radixSort.js
@@ -12,3 +12,16 @@ function radixSort(){
 function getDigit(num, position){
     return Math.floor(Math.abs(num) / Math.pow(10, position)) % 10
 }
+
+function digitCount(num){
+    if(num === 0) return 1
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(numbers){
+    let maxDigits = 0
+    for(let i = 0; i < numbers.length; i++){
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+    }
+    return maxDigits
+}
