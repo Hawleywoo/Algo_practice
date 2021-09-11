0f40d6f43dec7adda9a749d64d764b5dc7bbaedc
Hawleywoo <alhawley21@gmail.com>
2021-09-11 00:28:56 -0600

    Doubly push, pop, shift, unshift

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,20 +1,82 @@
-class Node{
-    constructor(val){
+class Node {
+    constructor(val) {
         this.val = val;
         this.next = null;
         this.prev = null;
     }
 }
 
-class DoublyLinkedList{
+class DoublyLinkedList {
 
-    constructor(){
+    constructor() {
         this.head = null
         this.tail = null
         this.length = 0
     }
 
-    push(){
-        
+    push(value) {
+        let newNode = new Node(value)
+
+        if (this.length === 0) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+
+        this.length++
+        return this
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+
+        let oldTail = this.tail
+
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = oldTail.prev
+            this.tail.next = null
+            oldTail.prev = null
+        }
+
+        this.length--
+        return oldTail
+    }
+
+    shift(){
+        if (!this.head) return undefined;
+
+        let oldHead = this.head
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        }else{
+            this.head = oldHead.next
+            this.head.prev = null
+            oldHead.next = null
+        }
+
+        this.length--
+        return oldHead
+    }
+
+    unshift(val){
+        let newNode = new Node(val)
+
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }
+        newNode.next = this.head
+        this.head.prev = newNode
+        this.head = newNode
+
+        length++
+        return this
     }
 }
