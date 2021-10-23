f376a884a2573a68b5dabdba5eb81d8f77875c1f
Hawleywoo <alhawley21@gmail.com>
2021-10-22 23:43:51 -0600

    BST base

---
+++ binarySearchTree.js
@@ -0,0 +1,27 @@
+// BST
+// one or two children
+// Sorted and data that can be compared
+// Every node to the left is less than parent
+// Every node to the right is greater than parent
+
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+}
+
+
+let tree = new BinarySearchTree()
+tree.root = new Node(10)
+tree.root.right = new Node(15)
+tree.root.left = new Node(7)
+tree.root.left.right = new Node(9)
