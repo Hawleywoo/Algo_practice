30cc79852897682615b457607a7883a249ffb2c9
Hawleywoo <alhawley21@gmail.com>
2021-11-19 22:31:09 -0700

    Insert dif version

--- binaryHeap.js
+++ binaryHeap.js
@@ -29,4 +29,23 @@ class MaxBinaryHeap{
             parentIndex = findParent(index)
         }
     }
+
+    // colts solution
+    // insert(value){
+    //     this.bubbleUp()
+    // }
+    
+    // bubbleUp(){
+    //     let index = this.values.length - 1
+    //     const element = this.values[index]
+    //     while(index < 0){
+    //         let parentIdx = Math.floor((index-1)/2)
+    //         let parent = this.values[parentIdx]
+    //         if(element <= parent)break
+
+    //         this.values[parentIdx] = element
+    //         this.values[index] = parent
+    //         index = parentIdx
+    //     }
+    // }
 }
